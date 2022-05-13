99e34f0f724ab8f76cb2b0c2343506de77faaabd
sandroassek <uni.sandro.assek@gmail.com>
2022-05-13 17:29:47 +0200

    added: draw line between the points -> function representation updated: Draw point by pressing RIGHT Mouse Button

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 from matplotlib.backend_bases import MouseButton
 import matplotlib.pyplot as plt
 
@@ -7,11 +6,11 @@ class FunctionPlotter:
     def __init__(self):
         self.x = []
         self.y = []
-        self.fig = plt.figure()
-        self.fig
+        self.fig, self.ax = plt.subplots(figsize=(10, 6))
+        plt.subplots_adjust(bottom=0.2)
 
         plt.xlim(-10, 10)
-        plt.ylim(-10, 10)
+        plt.ylim(-5, 5)
         plt.xlabel("X")
         plt.ylabel("Y")
 
@@ -19,22 +18,23 @@ class FunctionPlotter:
 
         plt.connect('button_press_event', self._on_mouse_click)
         self.fig.canvas.mpl_connect('close_event', self._on_window_close)
+
         plt.show()
 
     def _on_mouse_click(self, event):
-        if event.button is MouseButton.LEFT:
+        if event.button is MouseButton.RIGHT:
             clicked_x, clicked_y = event.xdata, event.ydata
             if len(self.x) == 0 and len(self.y) == 0:
                 self.x += [clicked_x]
-                self.y += [clicked_x]
+                self.y += [clicked_y]
                 plt.plot(clicked_x, clicked_y, 'bo')
             else:
-                curr_x = self.x[-1]
-                # check if new clicked x value is smaller than any other
-                if clicked_x > curr_x:
+                # check if new clicked x value is smaller than any other -> one x value has exactly one y value
+                if clicked_x > self.x[-1]:
+                    # draw line between the two selected points
+                    plt.plot([clicked_x, self.x[-1]], [clicked_y, self.y[-1]], 'bo', linestyle="-")
                     self.x += [clicked_x]
-                    self.y += [clicked_x]
-                    plt.plot(clicked_x, clicked_y, 'bo')
+                    self.y += [clicked_y]
                 else:
                     print('not possible')
             self.fig.canvas.draw()
